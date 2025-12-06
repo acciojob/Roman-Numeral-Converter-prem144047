7e4a4db76a9ad192993a5540bb4e08539dd38639
Prem Kumar <prem144047@gmail.com>
2025-12-06 20:39:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function convertToRoman(num) {
-  	const obj = [
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const obj = [
+        ['M', 1000], 
+        ['D', 500], 
+        ['C', 100], 
+        ['L', 50], 
+        ['X', 10], 
+        ['V', 5], 
+        ['I', 1]
     ];
 
-  //your code here
-	 let result = '';
+    let result = '';
     
     for (let [symbol, value] of obj) {
         while (num >= value) {
@@ -20,14 +19,22 @@ function convertToRoman(num) {
     }
     
     return result;
-
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
+// Event listener for the Convert button
+document.getElementById('convertButton').addEventListener('click', function() {
+    const inputNumber = parseInt(document.getElementById('numberInput').value);
+    const romanNumeral = convertToRoman(inputNumber);
+    document.getElementById('resultOutput').innerText = romanNumeral; // Display the result
+});
+
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. 
+// To run it with input 36, uncomment the following line
 // console.log(convertToRoman(36));
 
-
-
+// Example console tests - can be removed if not needed
+console.log(convertToRoman(14)); // Output: XIV
+console.log(convertToRoman(798)); // Output: DCCXCVIII
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
